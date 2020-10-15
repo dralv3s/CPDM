1548470e16939735a82197453c71de467084a046
Tatyane Xavier Prado <69535481+TatyaneXP@users.noreply.github.com>
2020-10-14 21:40:59 -0300

    Serviço de Mensagem

--- app/src/main/java/com/drdev/cpdm2s2020/Service/ReminderBroadcast.java
+++ app/src/main/java/com/drdev/cpdm2s2020/Service/ReminderBroadcast.java
@@ -1,6 +1,5 @@
 package com.drdev.cpdm2s2020.Service;
 
-import android.app.NotificationChannel;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -8,14 +7,20 @@ import android.content.Intent;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
+import com.drdev.cpdm2s2020.Model.TarefaModel;
 import com.drdev.cpdm2s2020.R;
 
+import java.util.HashMap;
+
 public class ReminderBroadcast extends BroadcastReceiver {
+
+    public static HashMap<Integer, TarefaModel> mapTarefas;
 
     @Override
     public void onReceive(Context context, Intent intent) {
 
-        NotifService.notificationService();
+        //TODO Criar uma validação para ver se o id que esta sendo chamado, bate as informacoes com o mapTarefas
+        //TODO Substituir as informacoes Pela informacao da Tarefa
 
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "notifyCPDM")
                 .setSmallIcon(R.drawable.common_google_signin_btn_icon_dark)
